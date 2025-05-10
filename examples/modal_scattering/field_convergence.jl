0ee6d90b6e48fd3a006a0d5643ab1733af2ab642
kevish <kevish.napal@student-cs.fr>
2025-05-10 18:01:09 +0200

    updated animation size [skip ci]

--- examples/modal_scattering/field_convergence.jl
+++ examples/modal_scattering/field_convergence.jl
@@ -28,7 +28,7 @@ microstructure = Microstructure(host_medium,[sp_MC_to_EF(sp_MC, radius_big_cylin
 cylinder = Material(Circle(radius_big_cylinder),microstructure);
 us = average_scattered_field(ω, region, source, cylinder; only_scattered_waves=true,basis_field_order=5);
 # plot(us, ω; field_apply=real,seriestype = :contour,c=:balance)
-expected_plot = plot(us, ω; field_apply=real,seriestype = :contour,c=:balance,legend=:none,size=(600,300),title="Predicted\nEnsemble Average\n");
+expected_plot = plot(us, ω; field_apply=real,seriestype = :contour,c=:balance,legend=:none,title="Predicted\nEnsemble Average\n");
 
 
 basis_order=3;
@@ -36,7 +36,7 @@ basis_order=3;
 x_vec, _ = points_in_shape(region;resolution=resolution);                            # space discretization 
 nb_of_configurations = 5;                                                   # total number of config 
 
-l = @layout [a b c; d]
+l = @layout [a b c; d{0.1h}]
 
 A = complex(zeros(length(x_vec),nb_of_configurations));                       # store the fields of each config
 anim = @animate for i ∈ 1:nb_of_configurations
@@ -45,22 +45,22 @@ anim = @animate for i ∈ 1:nb_of_configurations
     particles_realisation = renew_particle_configurations(sp_MC,radius_big_cylinder);
     sim = FrequencySimulation(particles_realisation,source);
     scattered_field = run(sim,region,[ω];only_scattered_waves=true,basis_order=basis_order,res=resolution);
-    sc_plot = plot(scattered_field,ω; field_apply=real,seriestype = :contour,c=:balance,title="\n Single Realisation\n",legend=:none,size=(5,5));
+    sc_plot = plot(scattered_field,ω; field_apply=real,seriestype = :contour,c=:balance,title="\n Single Realisation\n",legend=:none);
     
     # plot average scattered field
     A[:,i] = mean.(scattered_field.field[:,1])
     mean_A  = mean(A[:,1:i],dims=2);
     mean_us = FrequencySimulationResult(mean_A,x_vec,[ω]);
-    mean_sc_plot = plot(mean_us,ω; field_apply=real,seriestype = :contour,c=:balance,title="Cumulative\nEnsemble Average\n",legend=:none,size=(5,5));
+    mean_sc_plot = plot(mean_us,ω; field_apply=real,seriestype = :contour,c=:balance,title="Cumulative\nEnsemble Average\n",legend=:none);
 
     
     # Progress bar dimensions (bar background and fill)
     progress = i / nb_of_configurations
     bar_y = 0.0
-    progress_plot = plot([0, 2π], [bar_y, bar_y], lw=6, color=:lightgray, label = "", grid=false,axis=false, ylims=(-.01,.01))
+    progress_plot = plot([0, 2π], [bar_y, bar_y], lw=6, color=:lightgray, label = "", grid=false,axis=false, ylims=(-.01,.01),size=(1500, 100))
     progress_plot = plot!([0, 2π * progress], [bar_y, bar_y], lw=6, color=:gray, label = "")
     
-    plot(sc_plot,mean_sc_plot, expected_plot, progress_plot,layout=l,size=(1500,900))
+    plot(sc_plot,mean_sc_plot, expected_plot, progress_plot,layout=l,size=(1500,700))
 end
 gif(anim, "animation1.gif", fps = nb_of_configurations/10)
 
